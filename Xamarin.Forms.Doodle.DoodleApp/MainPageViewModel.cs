21c17f77fc4a6be575999ad0732d7e7f9c3a6a7a
Alexey Strakh <alexey.strakh@microsoft.com>
2018-06-15 15:39:48 -0700

    add basic logic with button command, label update and a viewmodel to handle the behavior

---
+++ Xamarin.Forms.Doodle.DoodleApp/MainPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Xamarin.Forms.Doodle.DoodleApp
+{
+    public class MainPageViewModel : BindableObject
+    {
+        private int _state;
+
+        public string Text => $"You clicked {_state} time(s). I need more!";
+
+        public Command ExecuteAction { get; }
+
+        public MainPageViewModel()
+        {
+            ExecuteAction = new Command(() => OnExecuteAction());
+        }
+
+        private void OnExecuteAction()
+        {
+            _state++;
+
+            OnPropertyChanged("Text");
+        }
+    }
+}
